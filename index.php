513c70140d44b09df9cadbad2d23824696e6e939
ilqar <ilqar.huseynli.9686@gmail.com>
2018-12-06 20:07:54 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
+<?php 
+	$test ="ilqar huseynli";
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>ilqar</title>
+	<title>
+		
+	</title>
 </head>
 <body>
-	<h1>salam ilqar</h1>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<?php echo $test; ?>
 </body>
 </html>
